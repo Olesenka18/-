d325bcb2f0b7e637d71bbf34db01bcfa7e9b023a
Olesenka18 <tolk.les18@gmail.com>
2020-09-30 00:23:08 +0300

    2

--- 4.1/4.1/4.1.cpp
+++ 4.1/4.1/4.1.cpp
@@ -2,14 +2,14 @@
 #include <locale.h>
 int main(void) {
 	setlocale(LC_ALL, "Russian");
-	int a, b, s, p;
+	float a, b, s, p;
 	printf("Введите значение переменной a \n a = ");
-	scanf("%i", &a);
+	scanf("%f", &a);
 	printf("Введите значение переменной b \n b = ");
-	scanf("%i", &b);
+	scanf("%f", &b);
 	s = a * b;
 	p = (a + b) * 2;
-	printf("Площадь равна %i \n", s);
-	printf("Периметр равен %i \n", p);
+	printf("Площадь равна %f \n", s);
+	printf("Периметр равен %f \n", p);
 	return 0;
 }
