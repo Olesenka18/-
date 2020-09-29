2ff166be1f5702f2eeb59a2f4ead4a27bad30599
Olesenka18 <tolk.les18@gmail.com>
2020-09-30 00:34:23 +0300

    1

---
+++ 4.3/4.3/4.3.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	float a, b, m;
+	printf("Введите значение переменной a \n a = ");
+	scanf("%f", &a);
+	printf("Введите значение переменной b \n b = ");
+	scanf("%f", &b);
+	m = (a + b) / 2;
+	printf("Среднее арифметическое чисел a и b равно %f \n", m);
+	return 0;
+}
+
