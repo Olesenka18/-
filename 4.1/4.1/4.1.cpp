f486cc4ac30a2fe16dc4879717125e11ce2f71bd
Olesenka18 <tolk.les18@gmail.com>
2020-09-29 23:55:02 +0300

    second commit

---
+++ 4.1/4.1/4.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int a, b, s, p;
+	printf("Введите значение переменной a \n a = ");
+	scanf("%i", &a);
+	printf("Введите значение переменной b \n b = ");
+	scanf("%i", &b);
+	s = a * b;
+	p = (a + b) * 2;
+	printf("Площадь равна %i \n", s);
+	printf("Периметр равен %i \n", p);
+	return 0;
+}
