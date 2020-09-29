f0aae7b8a2e2f0f70ef4874a80e5f3c7b3738319
Olesenka18 <tolk.les18@gmail.com>
2020-09-30 00:20:29 +0300

    1

---
+++ 4.2/4.2/4.2.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define π 3.14
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	float d, L;
+	printf("Введите диаметр \n d = ");
+	scanf("%f", &d);
+	L = d * π;
+	printf("Длина окружности равна %f \n", L);
+	return 0;
+}
