093da1659464f0c24c02b9f5c9bc49aa31ad9bdd
Olesenka18 <tolk.les18@gmail.com>
2020-10-01 20:18:09 +0300

    1

---
+++ 5.2/5.2/5.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	float A, B, C, AB, BC;
+	printf("Введите точку A \n A = ");
+	scanf("%f", &A);
+	printf("Введите точку B \n B = ");
+	scanf("%f", &B);
+	printf("Введите точку C \n C = ");
+	scanf("%f", &C);
+	AB = fabsf(A - B);
+	BC = fabsf(B - C);
+	printf("AB = %f ", AB);
+	printf("BC = %f ", BC);
+
+	return 0;
+
+}
