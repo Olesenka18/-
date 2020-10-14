754bd0bed10f4d20c2126c8e0971dd95498f0515
Olesenka18 <tolk.les18@gmail.com>
2020-10-14 17:16:50 +0300

    1

---
+++ 6.1/6.1/6.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	float A, B;
+	float C;
+	printf("Введите значение A \n");
+	scanf("%f", &A);
+	printf("Введите значение B \n");
+	scanf("%f", &B);
+	C = A;
+	A = B;
+	B = C;
+	printf("A = %f \n", A);
+	printf("B = %f \n", B);
+
+	return 0;
+
+}
