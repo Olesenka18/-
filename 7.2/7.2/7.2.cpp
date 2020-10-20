46d83b25f427bc318c62668d94dfee2155a1b7c9
Olesenka18 <tolk.les18@gmail.com>
2020-10-21 01:32:09 +0300

    1

---
+++ 7.2/7.2/7.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	float a;
+	printf("Введите значение a \n");
+	scanf("%f", &a);
+	
+	printf("a = %lf \n", a);
+	return 0;
+
+}
