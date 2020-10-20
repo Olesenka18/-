377ee8668bfac125f88b10f5f8b3f629d60ae5a0
Olesenka18 <tolk.les18@gmail.com>
2020-10-21 01:20:33 +0300

    1

---
+++ 7.1/7.1/7.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define pi 3.14
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	float a;
+	printf("Введите значение a \n");
+	scanf("%f", &a);
+	a = a * pi / 180;
+	printf("a = %lf \n", a);
+	return 0;
+
+}
