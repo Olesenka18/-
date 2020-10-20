e786a74fadfbc34f70e531dc72db8ab142332347
Olesenka18 <tolk.les18@gmail.com>
2020-10-21 00:20:10 +0300

    1

---
+++ 6.4/6.4/6.4.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	double y, x;
+	printf("Введите значение x \n");
+	scanf("%lf", &x);
+	y = 3 * pow(x, 6) - 6 * x * x - 7;
+	printf("y = %lf \n", y);
+	return 0;
+
+}
