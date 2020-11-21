b04eed5a945f7390d0e95be2a7c132894cd82e69
Olesenka18 <tolk.les18@gmail.com>
2020-11-21 22:23:30 +0300

    1

---
+++ 8/8.1/8.1/8.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	printf("Введите размер файла в байтах\n a = ");
+	scanf("%i", &a);
+	a = a / 1024;
+	printf("Размер файла в килобайтах %i \n", a);
+	return 0;
+
+}
