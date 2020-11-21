4439fd1891435388205cee9b03412d559dfa4876
Olesenka18 <tolk.les18@gmail.com>
2020-11-21 23:26:44 +0300

    1

---
+++ 8/8.4/8.4/8.4.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, X, P;
+	printf("Введите двузначное число A \n");
+	scanf("%i", &A);
+	X = A / 10;
+	P = A % 10;
+	B = P * 10 + X;
+	printf("Число, полученное при перестановке цифр, равно %i \n", B);
+	return 0;
+}
