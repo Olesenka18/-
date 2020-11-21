3b2bf4a712841fc9cce3f18bffc2712dd303a556
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 00:36:44 +0300

    1

---
+++ 9/9.5/9.5/9.5.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, N;
+	printf("Введите год \n A = ");
+	scanf("%i", &A);
+	N = ((A - 1) / 100) + 1;
+	printf("N = %i \n", N);
+	return 0;
+
+}
