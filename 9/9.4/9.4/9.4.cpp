35c8a2c3c920caa9059df9c51d407509f11981c4
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 00:27:03 +0300

    1

---
+++ 9/9.4/9.4/9.4.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C, N, P;
+	printf("Введите сторону A \n A = ");
+	scanf("%i", &A);
+	printf("Введите сторону B \n B = ");
+	scanf("%i", &B);
+	printf("Введите сторону C \n C = ");
+	scanf("%i", &C);
+	N = (A * B) / (C * C);
+	P = (A * B) % (C * C);
+	printf("Количество квадратов равно %i \n", N);
+	printf("Площадь оставшейся части равна %i \n", P);
+	return 0;
+
+}
