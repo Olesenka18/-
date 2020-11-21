c6cf8e7f4d4a35c921f26a6c43f7d2a54c3b10ca
Olesenka18 <tolk.les18@gmail.com>
2020-11-21 22:33:03 +0300

    1

---
+++ 8/8.2/8.2/8.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, X;
+	printf("Введите значение A \n");
+	scanf("%i", &A);
+	printf("Введите значение B \n");
+	scanf("%i", &B);
+	X = A / B;
+	printf("Количество отрезков равно %i \n", X);
+	return 0;
+}
