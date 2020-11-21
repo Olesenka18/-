0efe42fff9944a3e05dee5f17a00f277132f7cc5
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 01:21:15 +0300

    1

---
+++ 10/10.4/10.4/10.4.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C, D;
+	printf("Введите трёхзначное число A \n");
+	scanf("%i", &A);
+	B = A / 100;
+	C = A % 10;
+	D = (A / 10) % 10;
+	if (((B < D) && (D < C)) ^ ((B > D) && (D > C))) {
+		printf("Истина");
+	}
+	else {
+		printf("Ложь");
+	}
+
+	return 0;
+
+}
