d8dd8ce1fe1df36a3bb533e3393ddf7eb385954e
Olesenka18 <tolk.les18@gmail.com>
2020-11-21 23:38:00 +0300

    1

---
+++ 8/8.5/8.5/8.5.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, X, P;
+	printf("Введите трёхзначное число A \n");
+	scanf("%i", &A);
+	X = A / 100;
+	P = A % 100;
+	B = P * 10 + X;
+	printf("Полученное число равно %i \n", B);
+	return 0;
+}
