56878b5b2f4d4c5e984e2b2baf67b58ac7c3ec74
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 00:01:55 +0300

    1

---
+++ 9/9.2/9.2/9.2.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int K, N;
+	printf("Введите значение K \n");
+	scanf("%i", &K);
+	N = K % 7;
+	printf("Номер дня недели для %i дня равен %i \n", K, N);
+	return 0;
+
+}
