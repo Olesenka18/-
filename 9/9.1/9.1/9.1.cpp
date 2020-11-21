6d5b7e51093d1c20280a20e96e61df7c909fbbc8
Olesenka18 <tolk.les18@gmail.com>
2020-11-21 23:52:36 +0300

    1

---
+++ 9/9.1/9.1/9.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int N;
+	printf("Введите значение N \n");
+	scanf("%i", &N);
+	N = N % 60;
+	printf("C начала последней минуты прошло %i секунд  \n", N);
+	return 0;
+
+}
