7f3469d8479109fb48ce432e85bec0171d23fc64
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 01:37:32 +0300

    1

---
+++ 10/10.5/10.5/10.5.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C, D, F, K;
+	printf("Введите четырёхзначное число A \n");
+	scanf("%i", &A);
+	B = A / 100;
+	C = A % 100;
+	D = C / 10;
+	F = C % 10;
+	K = F * 10 + D;
+	if (B==K) {
+		printf("Истина");
+	}
+	else {
+		printf("Ложь");
+	}
+
+	return 0;
+
+}
