fcf4f0a6960703a6645277e74b1a5c152c032be7
Olesenka18 <tolk.les18@gmail.com>
2020-11-21 22:39:45 +0300

    1

---
+++ 8/8.3/8.3/8.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, X, P;
+	printf("Введите значение A \n");
+	scanf("%i", &A);
+	printf("Введите значение B \n");
+	scanf("%i", &B);
+	X = A / B;
+	P = A - B * X;
+	printf("Длина незанятой части равна %i \n", P);
+	return 0;
+}
