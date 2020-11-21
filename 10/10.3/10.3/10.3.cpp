83939f92419e09e694561d849bd9acb397156e63
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 01:05:32 +0300

    1

---
+++ 10/10.3/10.3/10.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C;
+	printf("Введите значение A \n");
+	scanf("%i", &A);
+	B = A % 2;
+	if ((B == 0) && (A>9) && (A < 100)) {
+		printf("Истина");
+	}
+	else {
+		printf("Ложь");
+	}
+
+	return 0;
+
+}
