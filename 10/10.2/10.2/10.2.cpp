7e2d17a2bc593cfa38db04c20b2a17c283d96235
Olesenka18 <tolk.les18@gmail.com>
2020-11-22 00:55:15 +0300

    1

---
+++ 10/10.2/10.2/10.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int A, B, C;
+	printf("Введите значение A \n");
+	scanf("%i", &A);
+	printf("Введите значение B \n");
+	scanf("%i", &B);
+	printf("Введите значение C \n");
+	scanf("%i", &C);
+	if ((A < B) && (B < C)) {
+		printf("Истина");
+	}
+	else {
+		printf("Ложь");
+	}
+
+	return 0;
+
+}
+
